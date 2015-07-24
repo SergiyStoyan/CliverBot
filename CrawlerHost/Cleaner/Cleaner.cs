48947de51533a7801f8423349e3e311aa28c3074
cliversoft <sergey.stoyan@gmail.com>
2015-07-24 04:06:17 +0300

    .

---
+++ CrawlerHost/Cleaner/Cleaner.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cliver.Bot;
+using System.Text.RegularExpressions;
+using Cliver.CrawlerHost;
+
+namespace Cliver.CrawlerHostCleaner
+{
+    class Cleaner
+    {
+        public static void Do()
+        {
+            //EmailRoutines.Send
+
+            DbApi.Connection.Get("DELETE FROM Messages WHERE Time<@OldTime").Execute("@OldTime", DateTime.Now.AddDays(-Properties.Settings.Default.DeleteMessagesOlderThanDays));
+
+            Recordset ps = DbApi.Connection.Get(@"SELECT name FROM sysobjects WHERE name LIKE 'PRODUCTS_%' and xtype='U'").GetRecordset();
+            foreach (Record p in ps)
+            {
+                if (null != DbApi.Connection["SELECT Id FROM Crawlers WHERE _ProductsTable=@ProductsTable"].GetSingleValue("@ProductsTable", p["name"]))
+                    continue;
+                DbApi.Message(DbApi.MessageType.WARNING, "Deleting table '" + p["name"] + "' as not crawler owning it.");
+                DbApi.Connection.Get("DROP TABLE @ProductsTable").Execute("@ProductsTable", p["name"]);
+            }
+        }
+    }
+}
---
+++ CrawlerHost/Cleaner/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cliver.Bot;
+using System.Text.RegularExpressions;
+
+namespace Cliver.CrawlerHostCleaner
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            try
+            {
+                LogMessage.DisableStumblingDialogs = ProgramRoutines.IsParameterSet(CommandLineParameters.AUTOMATIC);
+                Log.LOGGING_MODE = Log.LoggingMode.ONLY_LOG;
+                LogMessage.Output2Console = true;
+                ProcessRoutines.RunSingleProcessOnly();
+                Cleaner.Do();
+               CrawlerHost.ServiceApi.Complete(true);
+            }
+            catch (Exception e)
+            {
+                CrawlerHost.DbApi.Message(e);
+                CrawlerHost.ServiceApi.Complete(false);
+            }
+        }
+    }
+}
