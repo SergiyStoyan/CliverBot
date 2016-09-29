4fd4ffc3976c8af5674bee52a7739f01144a1e8a
cliversoft <sergey.stoyan@gmail.com>
2016-09-29 18:24:07 +0300

    .

--- CliverRoutines/Misc/AssemblyRoutines.cs
+++ CliverRoutines/Misc/AssemblyRoutines.cs
@@ -32,29 +32,15 @@ namespace Cliver
     {
         public static DateTime GetAssemblyCompiledTime(Assembly assembly)
         {
-            string filePath = assembly.Location;
-            const int c_PeHeaderOffset = 60;
-            const int c_LinkerTimestampOffset = 8;
-            byte[] b = new byte[2048];
-            System.IO.Stream s = null;
-
-            try
-            {
-                s = new System.IO.FileStream(filePath, System.IO.FileMode.Open, System.IO.FileAccess.Read);
-                s.Read(b, 0, 2048);
-            }
-            finally
-            {
-                if (s != null)
-                    s.Close();
-            }
-
-            int i = System.BitConverter.ToInt32(b, c_PeHeaderOffset);
-            int secondsSince1970 = System.BitConverter.ToInt32(b, i + c_LinkerTimestampOffset);
+            byte[] bs = new byte[2048];
+            System.IO.Stream s = new System.IO.FileStream(assembly.Location, System.IO.FileMode.Open, System.IO.FileAccess.Read);
+            s.Read(bs, 0, bs.Length);
+            s.Close();
+            int i = System.BitConverter.ToInt32(bs, 60);
+            int secs_since_1970 = System.BitConverter.ToInt32(bs, i + 8);
             DateTime dt = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-            dt = dt.AddSeconds(secondsSince1970);
-            dt = dt.ToLocalTime();
-            return dt;
+            dt = dt.AddSeconds(secs_since_1970);
+            return dt.ToLocalTime();
         }
     }
 }
