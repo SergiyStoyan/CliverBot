2f30108a75baf8d65480dd124d5c55376a4549b0
cliversoft <sergey.stoyan@gmail.com>
2016-09-29 15:27:42 +0300

    .

---
+++ CliverRoutines/Misc/AssemblyRoutines.cs
@@ -0,0 +1,60 @@
+﻿//********************************************************************************************
+//Author: Sergey Stoyan
+//        sergey.stoyan@gmail.com
+//        sergey_stoyan@yahoo.com
+//        http://www.cliversoft.com
+//        26 September 2006
+//Copyright: (C) 2006, Sergey Stoyan
+//********************************************************************************************
+using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Threading;
+using System.Configuration;
+using System.Media;
+using System.Web;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using System.Management;
+using System.Reflection;
+
+namespace Cliver
+{
+    public static class AssemblyRoutines
+    {
+        public static DateTime GetAssemblyCompiledTime(Assembly assembly)
+        {
+            string filePath = assembly.Location;
+            const int c_PeHeaderOffset = 60;
+            const int c_LinkerTimestampOffset = 8;
+            byte[] b = new byte[2048];
+            System.IO.Stream s = null;
+
+            try
+            {
+                s = new System.IO.FileStream(filePath, System.IO.FileMode.Open, System.IO.FileAccess.Read);
+                s.Read(b, 0, 2048);
+            }
+            finally
+            {
+                if (s != null)
+                    s.Close();
+            }
+
+            int i = System.BitConverter.ToInt32(b, c_PeHeaderOffset);
+            int secondsSince1970 = System.BitConverter.ToInt32(b, i + c_LinkerTimestampOffset);
+            DateTime dt = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            dt = dt.AddSeconds(secondsSince1970);
+            dt = dt.ToLocalTime();
+            return dt;
+        }
+    }
+}
