8189a78246c7661c6a11b1805b5c1dfabd977879
cliversoft <sergey.stoyan@gmail.com>
2017-03-30 13:07:10 +0300

    .

--- CliverRoutines/Misc/ProgramRoutines.cs
+++ CliverRoutines/Misc/ProgramRoutines.cs
@@ -28,6 +28,38 @@ namespace Cliver
 {
     public static class ProgramRoutines
     {
+        public static Dictionary<string, string> GetCommandLineParameters(char equal = '=')
+        {
+            Dictionary<string, string> clps = new Dictionary<string, string>();
+            if (equal == ' ')
+            {
+                string[] args = Environment.GetCommandLineArgs();
+                for (int i = 2; i < args.Length; i += 2)
+                    clps[args[i - 1]] = args[i];
+            }
+            else
+            {
+                string args = Regex.Replace(Environment.CommandLine, @"^(\"".*?\""|\'.*?\'|.*?)\s+", "");
+                for (Match m = Regex.Match(args, @"(?'Key'\w+?)" + Regex.Escape(equal.ToString()) + @"(?'Value'\"".*?\""|\'.*?\'|.*?)(\s|$)"); m.Success; m = m.NextMatch())
+                {
+                    string v = m.Groups["Value"].Value;
+                    if (v.Length > 1)
+                        if (v[0] == '"')
+                        {
+                            v = v.Trim('"');
+                            v = Regex.Replace(v, @"\\""", "\"");
+                        }
+                        else if (v[0] == '\'')
+                        {
+                            v = v.Trim('\'');
+                            v = Regex.Replace(v, @"\\\'", "'");
+                        }
+                    clps[m.Groups["Key"].Value] = v;
+                }
+            }
+            return clps;
+        }
+
         public class CommandLineParameters
         {
             public static readonly CommandLineParameters NOT_SET = new CommandLineParameters(null);
