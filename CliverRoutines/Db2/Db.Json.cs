b6cb6abf753ca9160d12e3cc8f07d21bfe547a7b
cliversoft <sergey.stoyan@gmail.com>
2017-05-20 20:36:06 +0300

    .

---
+++ CliverRoutines/Db2/Db.Json.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.IO;
+using System.Reflection;
+
+namespace Cliver
+{
+    public partial class Db2
+    {
+        public class Json
+        {
+            public abstract class Table<D>: Table where D : Document, new()
+            {
+                public Table()
+                {
+                }
+
+                protected List<D> table
+                {
+                    get
+                    {
+                        return (List<D>)get_table_info().Core;
+                    }
+                }
+                //protected List<D> table
+                //{
+                //    get
+                //    {
+                //        List<D> v;
+                //        if (!t.TryGetTarget(out v))
+                //        {
+                //            v = (List<D>)get_table_info().Core;
+                //            t.SetTarget(v);
+                //        }
+                //        return v;
+                //    }
+                //}
+                //WeakReference<List<D>> t = new WeakReference<List<D>>(null);
+
+                protected override object create_table_core()
+                {
+                    string file = db_dir + "\\" + Name + ".json";
+                    //if (!File.Exists(file))
+                    //{
+                    //    if (!Message.YesNo("The app needs data which should be downloaded over the internet. Make sure your computer is connected to the internet and then click Yes. Otherwise, the app will exit."))
+                    //        Environment.Exit(0);
+                    //    BeginRefresh(true);
+                    //    if (!File.Exists(file))
+                    //    {
+                    //        Message.Error("Unfrotunately the required data has not been downloaded. Please try later.");
+                    //        return new List<D>();
+                    //    }
+                    //}
+
+                    string s = System.IO.File.ReadAllText(file);
+                    return SerializationRoutines.Json.Deserialize<List<D>>(s);
+                }
+
+                public List<D> GetAll()
+                {
+                    lock (table)
+                    {
+                        return table.ToList();
+                    }
+                }
+
+                public List<D> Get(Func<D, bool> query)
+                {
+                    lock (table)
+                    {
+                        return table.Where(query).ToList();
+                    }
+                }
+
+                //static public void RefreshFile()
+                //{
+                //    throw new Exception("Not implemented");
+                //}
+
+                //protected static void refresh_json_file_by_request(string url)
+                //{
+                //    Type t = new System.Diagnostics.StackFrame(1).GetMethod().DeclaringType;
+                //    Log.Main.Inform("Refreshing table: " + t.Name);
+                //    HttpResponseMessage rm = http_client.GetAsync(url).Result;
+                //    if (!rm.IsSuccessStatusCode)
+                //        throw new Exception("Could not refresh table: " + rm.ReasonPhrase);
+                //    if (rm.Content == null)
+                //        throw new Exception("Response content is null.");
+                //    string s = rm.Content.ReadAsStringAsync().Result;
+                //    System.IO.File.WriteAllText(db_dir + "\\" + t.Name + ".json", s);
+                //}
+
+                //protected static void refresh_json_file_by_file(string file)
+                //{
+                //    Type t = new System.Diagnostics.StackFrame(1).GetMethod().DeclaringType;
+                //    Log.Main.Inform("Refreshing table: " + t.Name);
+                //    string[] ls = File.ReadAllLines(file);
+                //    string[] hs = ls[0].Split(',');
+                //    Dictionary<string, int> hs2i = new Dictionary<string, int>();
+                //    for (int i = 0; i < hs.Length; i++)
+                //        hs2i[hs[i]] = i;
+                //    PropertyInfo[] pis = typeof(D).GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly);
+                //    List<D> ds = new List<D>();
+                //    for (int i = 1; i < ls.Length; i++)
+                //    {
+                //        string[] vs = ls[i].Split(',');
+                //        D d = new D();
+                //        foreach (PropertyInfo pi in pis)
+                //            pi.SetValue(d, vs[hs2i[pi.Name]]);
+                //        ds.Add(d);
+                //    }
+                //    string s = SerializationRoutines.Json.Serialize(ds);
+                //    File.WriteAllText(db_dir + "\\" + t.Name + ".json", s);
+                //}
+            }
+        }
+    }
+}
---
+++ CliverRoutines/Db2/Db.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+
+namespace Cliver
+{
+    public partial class Db2
+    {
+        static readonly string db_dir = Log.GetAppCommonDataDir();
+        static readonly string db_file = db_dir + "\\db.litedb";
+        
+        static Db2()
+        {
+        }
+
+        static public Modes Mode
+        {
+            set
+            {
+                mode = value;
+                switch (mode)
+                {
+                    case Modes.CLOSE_TABLE_ON_DISPOSE:
+                        break;
+                    case Modes.KEEP_ALL_OPEN_TABLES_FOREVER:
+                        break;
+                    case Modes.KEEP_ALL_OPEN_TABLES_WHILE_AT_LEAST_ONE_TABLE_IN_USE:
+                        break;
+                    default:
+                        throw new Exception("No option: " + mode);
+                }
+            }
+            get
+            {
+                return mode;
+            }
+        }
+        static Modes mode = Modes.CLOSE_TABLE_ON_DISPOSE;
+
+        public enum Modes
+        {
+            KEEP_ALL_OPEN_TABLES_FOREVER,//requires explicite call Close()
+            KEEP_ALL_OPEN_TABLES_WHILE_AT_LEAST_ONE_TABLE_IN_USE,
+            CLOSE_TABLE_ON_DISPOSE,
+        }
+
+        static public void Close()
+        {
+            lock (table_types2table_info)
+            {
+                table_types2table_info.Clear();
+            }
+        }
+        
+        public class Document
+        {
+            public int Id { get; set; }
+        }
+
+        public abstract class Table : IDisposable
+        {
+            public Table()
+            {
+                Name = GetType().Name;
+                get_table_info().Count++;
+            }
+            public readonly string Name;
+
+            protected TableInfo get_table_info()
+            { 
+                lock (table_types2table_info)
+                {
+                    TableInfo ti;
+                    if (!table_types2table_info.TryGetValue(GetType(), out ti))
+                    {
+                        ti = new TableInfo { Count = 0, Core = create_table_core() };
+                        table_types2table_info[GetType()] = ti;
+                    }
+                    return ti;
+                }
+            }
+
+            protected abstract object create_table_core();
+
+            ~Table()
+            {
+                Dispose();
+            }
+
+            virtual public void Dispose()
+            {
+                lock (table_types2table_info)
+                {
+                    TableInfo ti;
+                    if (!table_types2table_info.TryGetValue(GetType(), out ti))
+                        return;
+                    ti.Count--;
+                    switch (mode)
+                    {
+                        case Modes.CLOSE_TABLE_ON_DISPOSE:
+                            if (ti.Count < 1)
+                                table_types2table_info.Remove(GetType());
+                            break;
+                        case Modes.KEEP_ALL_OPEN_TABLES_FOREVER:
+                            break;
+                        case Modes.KEEP_ALL_OPEN_TABLES_WHILE_AT_LEAST_ONE_TABLE_IN_USE:
+                            foreach (TableInfo t in table_types2table_info.Values)
+                                if (t.Count > 0)
+                                    return;
+                            table_types2table_info.Clear();
+                            break;
+                        default:
+                            throw new Exception("No option: " + mode);
+                    }
+                }
+            }
+        }
+        public class TableInfo
+        {
+            public int Count = 0;
+            public object Core = null;
+        }
+        static readonly Dictionary<Type, TableInfo> table_types2table_info = new Dictionary<Type, TableInfo>();
+
+        public static string GetNormalized(string s)
+        {
+            if (s == null)
+                return null;
+            return System.Text.RegularExpressions.Regex.Replace(s.ToLower(), @" +", " ").Trim();
+        }
+    }
+}
